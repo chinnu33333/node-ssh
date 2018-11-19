900c848840021f3be96bfbd904a2da053c59551b
chinnu <dammyd8888@gmail.com>
2018-11-19 14:13:18 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+app.use((req,res,next)=>{
+ var low = new Date().toString();
+ var log =`${low}: ${req.method} ${req.url}`;
+ console.log(log);
+ fs.appendFile('Server.log',log +
+  '\n');
+ next();
+});
+app.use((req,res,next)=>{
+  res.render('maintain.hbs');
+});
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+// /bad - send back json with errorMessage
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
